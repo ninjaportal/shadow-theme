003ebaa7d98c3b77e0c1ed75fa38d03dc62b9545
= <youssef.afify122@gmail.com>
2024-10-13 16:14:41 +0300

    Fixes

--- src/Commands/ShadowInstallCommand.php
+++ src/Commands/ShadowInstallCommand.php
@@ -3,6 +3,7 @@
 namespace NinjaPortal\Shadow\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Process;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
@@ -15,6 +16,8 @@ class ShadowInstallCommand extends Command
 
     public function handle(): void
     {
+        $this->info('Checking and removing the default route from web.php...');
+        $this->removeDefaultRoute();
 
         // copying assets and views
         $this->info('Publishing assets ...');
@@ -23,6 +26,38 @@ class ShadowInstallCommand extends Command
         $this->call('vendor:publish', ['--tag' => 'shadow-theme-views', '--force' => true]);
         $this->info('Publishing js ...');
     }
+
+    /**
+     * Remove the default Laravel welcome route from web.php.
+     */
+    protected function removeDefaultRoute(): void
+    {
+        $webRoutePath = base_path('routes/web.php');
+
+        if (File::exists($webRoutePath)) {
+            // Load the contents of web.php
+            $content = File::get($webRoutePath);
+
+            // Define the default route content to search and remove
+            $defaultRoute = "Route::get('/', function () {\n    return view('welcome');\n});\n";
+
+            // Check if the default route exists in the file
+            if (str_contains($content, $defaultRoute)) {
+                // Remove the default route from the file content
+                $newContent = str_replace($defaultRoute, '', $content);
+
+                // Save the modified content back to web.php
+                File::put($webRoutePath, $newContent);
+
+                $this->info('Default route removed successfully.');
+            } else {
+                $this->info('No default route found.');
+            }
+        } else {
+            $this->error('routes/web.php not found.');
+        }
+    }
+
 
     public function npmInstall(string $package): void
     {
